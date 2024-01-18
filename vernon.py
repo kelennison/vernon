20e376b23d09d67562798a086be58e683da84918
kelennison <121885421+kelennison@users.noreply.github.com>
2024-01-18 23:53:45 +0000

    Update vernon.py

--- vernon.py
+++ vernon.py
@@ -99,7 +99,7 @@ def contact():
     st.title("Contact Us")
 
     contact_form = """
-    <form action="https://formsubmit.co/kelennison@gmail.com" method="POST">
+    <form action="https://formsubmit.co/novogroupgh@outlook.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
